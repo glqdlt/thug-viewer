1f31746f6b8e1fcebdb55f8d6625047e85f9d658
dlfdnd0725@gmail.com <dlfdnd0725@gmail.com>
2018-03-26 17:33:18 +0900

    Project Init

---
+++ src/main/java/com/glqdlt/thugviewer/ThugViewerApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.thugviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThugViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThugViewerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/glqdlt/thugviewer/ThugViewerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.glqdlt.thugviewer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThugViewerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
