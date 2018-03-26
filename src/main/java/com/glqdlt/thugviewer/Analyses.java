8680110fc3db0799b26b42c2400ad875da70a6e6
dlfdnd0725@gmail.com <dlfdnd0725@gmail.com>
2018-03-26 17:51:12 +0900

    update : Thugmodel -> analyses

--- src/main/java/com/glqdlt/thugviewer/Analyses.java
+++ src/main/java/com/glqdlt/thugviewer/Analyses.java
@@ -9,13 +9,16 @@ import org.springframework.data.mongodb.core.mapping.Document;
  * Created By iw.jhun
  * On 2018-03-26 , 오후 5:39
  */
-@Document(collection = "codes")
+@Document(collection = "analyses")
 @ToString
 @Data
-public class ThugModel {
+public class Analyses {
 
     @Id
     public String id;
 
+    public String url_id;
+
+    public String timestamp;
 
 }
--- src/main/java/com/glqdlt/thugviewer/ThugRepo.java
+++ src/main/java/com/glqdlt/thugviewer/ThugRepo.java
@@ -6,5 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * Created By iw.jhun
  * On 2018-03-26 , 오후 5:34
  */
-public interface ThugRepo extends MongoRepository<ThugModel,String> {
+public interface ThugRepo extends MongoRepository<Analyses,String> {
+
 }
